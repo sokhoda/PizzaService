7c6245110fdc8adff3f44ec8cc7b24bcae7d2c2d
Oleksandr Khodakovskyi <Oleksandr_khodakovskyi@epam.com>
2017-01-12 12:34:37 +0200

    add: MiscRunner.java

---
+++ src/main/java/infrastructure/MiscRunner.java
@@ -0,0 +1,49 @@
+package infrastructure;
+
+import org.springframework.web.context.WebApplicationContext;
+
+import java.lang.reflect.Method;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.StringJoiner;
+import java.util.TreeSet;
+
+public class MiscRunner {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder("abcdefg");
+        try {
+            String fd = "     gbb    ";
+            System.out.println("try");
+            System.out.println(builder.reverse());
+            System.out.println(fd.trim() + "!");
+            System.out.println(fd + "!");
+            System.out.println(fd.replaceAll("bb","aa"));
+        }
+        finally {
+            System.out.println("finally");
+        }
+        System.out.println("text1");
+
+    Set<Integer> set = new TreeSet<>(new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return 0;
+        }
+    });
+
+    Thread thread = new Thread(new Runnable() {
+        String str = "team";
+        public void printt(){
+            System.out.println("!");
+        }
+        @Override
+        public void run() {
+            printt();
+            System.out.println("hallo from " + str);
+        }
+    });
+
+    thread.start();
+
+    }
+}
