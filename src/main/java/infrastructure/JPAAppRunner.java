d61687798f859e46c39e4893c521338075b003b2
Oleksandr_Khodakovsk <oleksandr_khodakovskyi@epam.com>
2017-02-28 20:06:38 +0200

    persistence continued;

--- src/main/java/infrastructure/JPAAppRunner.java
+++ src/main/java/infrastructure/JPAAppRunner.java
@@ -1,5 +1,7 @@
 package infrastructure;
 
+import domain.Cheque;
+import domain.Orders;
 import domain.Pizza;
 import domain.PizzaType;
 import org.springframework.transaction.TransactionDefinition;
@@ -40,11 +42,20 @@ public class JPAAppRunner {
         System.out.println("before persist " +  pizza.getPizzaId());
         System.out.println("contains before=" + em.contains(pizza));
         em.persist(pizza);
+
+        Cheque cheque = em.find(Cheque.class, 1L);
+        System.out.println("_____________________________");
+        cheque.getDiscountList();
+
         Pizza pizza2 = em.getReference(Pizza.class, 2L);
+//        Orders aorders2 = em.getRefqerence(Orders.class, 2L);
 
 //        em.merge(pizza);
 //        pizza.setStrName("224324234");
-        System.out.println(pizza2);
+//        System.out.println("orders2=" + aorders2);
+//        aorders2.getCustomer();
+
+//        System.out.println("pizza2=" + pizza2);
         System.out.println("after persist " +  pizza.getPizzaId());
         System.out.println("contains after=" + em.contains(pizza));
 //        em.persist(oak);
@@ -55,10 +66,23 @@ public class JPAAppRunner {
 
         et.begin();
         Pizza pizzaL = em.find(Pizza.class, 5L);
-        pizzaL.setPrice(344.2);
-        em.refresh(pizzaL);
+        pizzaL.setPrice(44.2);
+//        em.refresh(pizzaL);
         System.out.println("Pizza " + pizzaL );
+        System.out.println(em.contains(pizzaL));
+//        em.detach(pizzaL);
+//        System.out.println("detached Pizza " + pizzaL );
+//        System.out.println(em.contains(pizzaL));
+//        em.merge(pizzaL);
+//        System.out.println("merged Pizza " + pizzaL );
+//        System.out.println(em.contains(pizzaL));
         et.commit();
+
+        System.out.println("contains==" + em.contains(pizzaL));
+        em.clear();
+        System.out.println("contains==" + em.contains(pizzaL));
+
+
 
         Pizza pizza1 = em.find(Pizza.class, 5L);
         System.out.println(pizza == pizza1);
